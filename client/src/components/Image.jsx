987bb384cc816cd5498bcfe8865a51139b16e6fe
gigicupp <gigicupp@gmail.com>
2021-01-30 20:56:27 -0800

    fixed the search icon. the opacity changes on hover

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -3,16 +3,38 @@ import styled from 'styled-components';
 import Popup from 'reactjs-popup';
 import { FaSistrix } from 'react-icons/fa';
 
+const Icon = styled.div`
+  opacity: 0;
+  transition: opacity .3s cubic-bezier(.235,0,.05,.95);
+  background-color: rgb(255 255 255 / .2);
+  border-radius: 1000px;
+  font-weight: bold;
+  width: 2.5rem;
+  height: 2.5rem;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  z-index: 1000;
+  left: calc(50%);
+  top: calc(50%);
+  /* &&:hover {
+    opacity: 1;
+  } */
+  `
+
 const Picture = styled.img`
   width: 100%;
   max-height: 100%;
   object-fit: cover;
   border-radius: 10px;
-  overflow: hidden;
   cursor: pointer;
   transition: opacity 1s,transform 1s cubic-bezier(.395,.005,.19,1),filter 1s;
+  overflow: hidden;
+
   &&:hover {
-    transform: scale(1.05)
+    transform: scale(1.05);
   }
 `
 const Video = styled.video`
@@ -33,29 +55,16 @@ const Wrapper = styled.div`
     flex-direction: column;
   }
 `
+
 const Container = styled.div`
   border-radius: 10px;
   overflow: hidden;
-  margin: 10px;
+  margin: 10px 5px 10px 5px;
   width: 100%;
-  box-sizing: border-box;
   flex: 1 0 40%;
-`
-
-const Icon = styled.div`
-  opacity: 0.5;
-  transition: opacity .3s cubic-bezier(.235,0,.05,.95);
-  background-color: #fff;
-  border-radius: 1000px;
-  font-weight: bold;
-  width: 4.6rem;
-  height: 4.6rem;
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  z-index: 1000;
+  &&:hover ${Icon} {
+    opacity: 1;
+  }
 `
 
 class Image extends React.Component {
@@ -69,7 +78,7 @@ class Image extends React.Component {
       if (index < 4) {
         return (
           <Container>
-            <Icon><FaSistrix size='1.25rem' fontWeight='bold' /></Icon>
+            <Icon><FaSistrix size='1.25rem' fontWeight='bold' background='transparent'/></Icon>
             <Picture src={`https://${image}`} alt="" key={index} />
           </Container>
         )
@@ -97,3 +106,4 @@ class Image extends React.Component {
 };
 
 export default Image;
+
