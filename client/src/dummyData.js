65ea5c879aecd9f657a912058311d7ad2b41b697
gigicupp <gigicupp@gmail.com>
2021-02-03 00:01:35 -0800

    added a new product image for ID# 2

--- client/src/dummyData.js
+++ client/src/dummyData.js
@@ -8,6 +8,16 @@ module.exports = [
       'www.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw0dad9aa7/images/hi-res/21217_INBK_SB1.jpg?sw=350&sh=350&sfrm=png&q=95&bgcolor=f6f6f6',
       'www.patagonia.com/media/videos/fit_motion/spring_2020/21217_SMDB_FIT_MOTION1.mp4'
     ]
+  },
+  {
+    "productId": 2,
+    "detailImages": [
+      'www.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw8392db4b/images/hi-res/22801_ELKH_OM1.jpg?sw=500&sh=500&sfrm=png&q=95&bgcolor=f6f6f6',
+      'www.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwf3c41439/images/hi-res/22801_ELKH_OM2.jpg?sw=500&sh=500&sfrm=png&q=95&bgcolor=f6f6f6',
+      'www.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dwd7bed536/images/hi-res/22801_BLK_OPEN.jpg?sw=500&sh=500&sfrm=png&q=95&bgcolor=f6f6f6',
+      'www.patagonia.com/dw/image/v2/BDJB_PRD/on/demandware.static/-/Sites-patagonia-master/default/dw4d9f916c/images/hi-res/22801_ELKH_OPEN.jpg?sw=500&sh=500&sfrm=png&q=95&bgcolor=f6f6f6',
+      'www.patagonia.com/media/videos/fit_motion/fall_2020/22801_ELKH_FIT_MOTION1.mp4'
+    ]
   }
 ]
 
